29e6ab85b51741bf63962e0adf4f4ee146e6aba0
Luke Schoen <ltfschoen@gmail.com>
2017-01-08 13:53:24 +1100

    Setup Sequelize ORM instance, relational db table schema and associations for PSQL. Faker fixtures init db.

---
+++ config/db.js
@@ -0,0 +1,7 @@
+export default {
+  schema: 'graphql_sequelize', // created with command `createdb graphql_sequelize`
+  username: 'master',
+  password: '',
+  dialect: 'postgres',
+  host: 'localhost'
+}
---
+++ db.js
@@ -0,0 +1,71 @@
+import Sequelize from 'sequelize';
+import db from './config/db';
+import _ from 'lodash';
+import Faker from 'faker';
+
+const SequelizeConnection = new Sequelize(
+  db.schema,
+  db.username,
+  db.password,
+  {
+    dialect: db.dialect,
+    host: db.host
+  }
+);
+
+function removeSpaces(val) {
+  return val.replace(/\s+/, '')
+}
+
+// Sequelize (ORM) table definitions for PostgreSQL database
+const Container = SequelizeConnection.define('container', {
+  id: {
+    type: Sequelize.STRING,
+    primaryKey: true,
+    unique: true,
+    allowNull: false,
+    set: function(val) {
+      this.setDataValue('id', removeSpaces(val));
+    }
+  }
+});
+
+const Section = SequelizeConnection.define('section', {
+  id: {
+    type: Sequelize.STRING,
+    primaryKey: true,
+    unique: true,
+    allowNull: false,
+    set: function(val) {
+      this.setDataValue('id', removeSpaces(val));
+    }
+  }
+});
+
+// Associations defined between resource tables
+Container.hasMany(Section);
+Section.belongsTo(Container);
+
+/**
+ * Synchronise the Sequelize models and associations with PostgreSQL database.
+ * Reinitializes the PostgreSQL tables each time run.
+ * Fixtures (fake data) after tables initialised.
+ * force checks if tables already exist but just overrides if false
+ */
+
+SequelizeConnection.sync({force: true}).then(() => {
+  _.times(10, () => {
+    return Container.create({
+      id: Faker.company.companyName()
+    }).then(container => {
+      return container.createSection({
+        id: Faker.name.firstName() + Faker.name.lastName()
+      });
+    }).catch(function(error) {
+      console.log("Error: ", error);
+    });
+  })
+});
+
+// Module to import into GraphQL Schema
+export default SequelizeConnection;
